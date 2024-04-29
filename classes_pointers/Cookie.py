a6cffb39d2b88c138505c5b275ba07f8ae161b28
Jonathan Heaney <jonathan.heaney@gmail.com>
2024-04-29 11:46:43 -0500

    Classes notes

---
+++ classes_pointers/Cookie.py
@@ -0,0 +1,25 @@
+# If a function has a 'self' keyword, then it's a method that's part of a class
+# You have a constructor function to create instances of the class
+# Then you can also add other methods in addition to the constructor
+
+class Cookie:
+    def __init__(self, color):
+        self.color = color
+
+    def get_color(self):
+        return self.color
+
+    def set_color(self, color):
+        self.color = color
+
+
+cookie_one = Cookie('green')
+cookie_two = Cookie('blue')
+
+print('Cookie one is', cookie_one.get_color())
+print('Cookie two is', cookie_two.get_color())
+
+cookie_one.set_color('yellow')
+
+print('\nCookie one is now', cookie_one.get_color())
+print('Cookie two is still', cookie_two.get_color())
